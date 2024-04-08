4a0cbe349e723b75d17932f33b9222d556d5b1e0
Ea Nasir <falli.matteo@itismeucci.com>
2024-04-08 13:37:58 +0200

    aggiornato index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
-    <title>Recensioni</title>
+    <title>Elimina recensioni</title>
 </head>
 <body>
     <h1>Elimina recensioni</h1>
